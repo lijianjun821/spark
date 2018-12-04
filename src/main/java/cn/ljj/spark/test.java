71bf9070ae920e1234d9f4b194e021b947258dd6
123 <123@163.com>
2018-12-04 14:47:59 +0800

    创建spark项目

---
+++ src/main/java/cn/ljj/spark/test.java
@@ -0,0 +1,10 @@
+package cn.ljj.spark;
+
+/**
+ * Created by QDHL on 2018/12/4.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println(123441);
+    }
+}
